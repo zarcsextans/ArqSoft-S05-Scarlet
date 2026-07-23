2574d77d52cf1866a146aad2140c9f966ab16c71
ScarletRuelas <Zarcsextans@gmail.com>
2026-07-22 21:23:53 -0600

    feat: rama del pipeline-ci

---
+++ CitasApp.Domain/Factories/CitasFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CitasApp.Domain.Factories
+{
+    internal class CitasFactory
+    {
+    }
+}
---
+++ CitasApp.Tests/Factories/CitaFactoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class CitaFactoryTests
+{
+    [Fact]
+    public void Construir_ConDatosValidos_CreaCitaConEstadoPendiente()
+    {
+        // Arrange
+        var factory = new CitaFactory();
+        // Act
+        var cita = factory.Construir(pacienteId: 1, medicoId: 2,
+        fecha: new DateOnly(2026, 7, 20), hora: new TimeOnly(10, 0), motivo: "Consulta");
+        // Assert
+        Assert.Equal("Pendiente", cita.Estado);
+        Assert.Equal(1, cita.PacienteId);
+    }
+}
